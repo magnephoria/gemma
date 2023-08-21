62cd78e11db4d02638133de9bc15d7f1ba77c2bf
Dennis <10742432+magnephoria@users.noreply.github.com>
2023-08-21 14:43:03 +0200

    Add pattern 'offset-headline'

---
+++ patterns/offset-headline.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Title: Offset headline
+ * Slug: gemma/offset-headline
+ * Categories: featured
+ * Block Types: core/heading
+ */
+?>
+<!-- wp:group {"align":"full","layout":{"type":"default"}} -->
+<div class="wp-block-group alignfull">
+    <!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+    <div class="wp-block-group"><!-- wp:heading {"align":"full","fontSize":"xx-large","fontFamily":"uncut-sans"} -->
+        <h2 class="wp-block-heading alignfull has-uncut-sans-font-family has-xx-large-font-size">Create your website</h2>
+        <!-- /wp:heading -->
+
+        <!-- wp:heading {"align":"wide","textColor":"contrast","fontSize":"xx-large"} -->
+        <h2 class="wp-block-heading alignwide has-contrast-color has-text-color has-xx-large-font-size">with simple elegance.</h2>
+        <!-- /wp:heading -->
+    </div>
+    <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
