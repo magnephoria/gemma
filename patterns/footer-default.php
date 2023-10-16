fc7aaa9fe9265f94228741ab265892e896c6e922
Dennis <10742432+magnephoria@users.noreply.github.com>
2023-10-16 22:00:21 +0200

    Fix header + footer for latest Gutenberg

--- patterns/footer-default.php
+++ patterns/footer-default.php
@@ -7,13 +7,13 @@
  */
 ?>
 
-<!-- wp:group {"style":{"spacing":{"margin":{"top":"var:preset|spacing|80"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--80)">
-    <!-- wp:group {"align":"full","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between","verticalAlignment":"top"}} -->
-    <div class="wp-block-group alignfull">
-        <!-- wp:site-title {"level":0} /-->
+<!-- wp:group {"align":"full","style":{"spacing":{"margin":{"top":"0","top":"var:preset|spacing|80"},"padding":{"left":"var:preset|spacing|40","right":"var:preset|spacing|40"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group alignfull"
+    style="margin-top:0;margin-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)">
+    <!-- wp:group {"align":"full","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
+    <div class="wp-block-group alignfull"><!-- wp:site-title {"level":0} /-->
 
-        <!-- wp:navigation {"__unstableLocation":"primary","overlayMenu":"never","layout":{"type":"flex","setCascadingProperties":"true","justifyContent":"left","orientation":"horizontal","flexWrap":"wrap"}} -->
+        <!-- wp:navigation {"overlayMenu":"never","layout":{"type":"flex","setCascadingProperties":"true","justifyContent":"left","orientation":"horizontal","flexWrap":"wrap"}} -->
             <!-- wp:navigation-link {"label":"Stellaris","url":"#","kind":"custom","isTopLevelLink":true} /-->
             
             <!-- wp:navigation-link {"label":"Lunavis","url":"#","kind":"custom","isTopLevelLink":true} /-->
