3859e192119fdc81e7aabbd7745b1cbac0a44738
Dennis <10742432+magnephoria@users.noreply.github.com>
2024-10-03 19:34:46 +0200

    Clean up patterns

--- patterns/footer-default.php
+++ patterns/footer-default.php
@@ -7,10 +7,12 @@
  */
 ?>
 
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"left":"var:preset|spacing|40","right":"var:preset|spacing|40"}}}},"layout":{"type":"default"}} -->
-<div class="wp-block-group alignfull" style="padding-right:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)">
+<!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|40","left":"var:preset|spacing|40"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group"
+    style="padding-right:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)">
     <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"}}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
-    <div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20)">
+    <div class="wp-block-group alignfull"
+        style="padding-top:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20)">
         <!-- wp:site-title {"level":0} /-->
 
         <!-- wp:navigation {"overlayMenu":"never","layout":{"type":"flex","setCascadingProperties":"true","justifyContent":"left","orientation":"horizontal","flexWrap":"wrap"}} -->
